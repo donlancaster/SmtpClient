f4f48f3c7a75dda53c9cec72c36012fa88824576
daniil <71727106+donlancaster@users.noreply.github.com>
2021-06-07 20:05:53 +0300

    Add files via upload

---
+++ quickmailer/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.Threading;
+using System.Collections;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace quickmailer
+{
+    public partial class Form1 : Form
+    {
+        private Workbook eWorkBook;
+        private Worksheet eWorkSheet;
+        private Microsoft.Office.Interop.Excel.Application excelApplication;
+        private static MailMessage[] mailMessages;
+        private Mailer[] mailers;
+        private NetworkCredential networkCredential;
+        private long mail_count = 0;
+        const long MAX_COUNT = 20;
+
+
+        public static void DeleteMessage(int index)
+        {
+            mailMessages[index].Dispose();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtUsername.Text = "";
+            txtPassword.Text = "";
+            txtSubject.Text = "";
+            cbName.Items.Add((string)"");
+            cbName.Items.Add((string)"");
+            cbName.SelectedIndex = 0;
+            networkCredential = new NetworkCredential(txtUsername.Text, txtPassword.Text);
+            mailers = new Mailer[MAX_COUNT];
+            mailMessages = new MailMessage[MAX_COUNT];
+        }
+
+        private void mnuOpenSource_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Excel 2007|*.xlsx|Excel 2003|*.xls";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                excelApplication = new Microsoft.Office.Interop.Excel.Application();
+                eWorkBook = excelApplication.Workbooks.Open(ofd.FileName);
+                eWorkSheet = eWorkBook.Worksheets[1];
+                int rs;
+
+                mainlist.Items.Clear();
+                ListViewItem listView;
+                rs = 2;
+                while (eWorkSheet.Cells[rs, 1].Value != null)
+                {
+                    String s = (string)eWorkSheet.Cells[rs, 2].Value;
+                    listView = mainlist.Items.Add(s);
+                    listView.SubItems.Add((string)eWorkSheet.Cells[rs, 3].Value);//attachments
+                    listView.SubItems.Add((string)eWorkSheet.Cells[rs, 4].Value);
+                    listView.SubItems.Add((string)eWorkSheet.Cells[rs, 5].Value);//email
+                    listView.SubItems.Add((string)eWorkSheet.Cells[rs, 6].Value);
+                    listView.SubItems.Add((string)eWorkSheet.Cells[rs, 7].Value);
+                    listView.SubItems.Add("");
+                    rs++;
+                }
+                eWorkBook.Close();
+                excelApplication.Quit();
+            }
+        }
+
+        private void mnuOpenMessageBody_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Message Body|*.txt";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader sr;
+                sr = new StreamReader(ofd.FileName);
+                sr.Close();
+            }
+        }
+
+        private void mnuSend_Click(object sender, EventArgs e)
+        {
+            string s_message;
+            string[] s_emails;
+            s_message = (File.OpenText(txtContent.Text)).ReadToEnd();
+
+            mail_count = 0;
+            foreach (ListViewItem item in mainlist.Items)
+            {
+                if (item.Checked)
+                {
+                    /*if (!File.Exists(lv.SubItems[2].Text))
+                    {
+                        lv.SubItems[3].Text = "No attachment!";
+                        continue;
+                    }*/
+
+                    if (item.SubItems[5].Text.Length == 0)
+                    {
+                        item.SubItems[5].Text = "No email address!";
+                        continue;
+                    }
+
+
+                    s_emails = item.SubItems[5].Text.Split(',', ';');
+
+
+                    foreach (string se in s_emails)
+                    {
+                        item.SubItems[6].Text = "Sending...";
+                        mailers[mail_count] = new Mailer(networkCredential);
+                        mailMessages[mail_count] = new MailMessage(txtEmail.Text, se);
+                        mailMessages[mail_count].From = new MailAddress(txtEmail.Text, cbName.Text);
+
+                        mailMessages[mail_count].Body = s_message;
+                        mailMessages[mail_count].Subject = txtSubject.Text + " FOR " + item.Text.ToUpper();
+                        mailMessages[mail_count].IsBodyHtml = true;
+                        mailMessages[mail_count].BodyEncoding = ASCIIEncoding.Default;
+
+                        if (item.SubItems[2].Text.Length!=0)
+                        {
+                            Attachment a = new Attachment(item.SubItems[2].Text);
+                            mailMessages[mail_count].Attachments.Add(a);
+                        }
+                        ListViewX sending = new ListViewX();
+                        sending.item = item;
+                        sending.rowindex = (int)mail_count;
+                        mailers[mail_count].SendMessage(mailMessages[mail_count], sending);
+                        mail_count++;
+                    }
+                }
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            networkCredential.UserName = txtUsername.Text;
+            networkCredential.Password = txtPassword.Text;
+            MessageBox.Show("Credentials changed!");
+        }
+
+        private void txtContent_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "HTML|*.html|HTM|*.htm";
+
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtContent.Text = ofd.FileName;
+            }
+        }
+
+        private void cbName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbName.SelectedIndex)
+            {
+                case 0:
+                    txtEmail.Text = "";
+                    break;
+
+                case 1:
+                    txtEmail.Text = "";
+                    break;
+            }
+        }
+    }
+
+    class Mailer
+    {
+        private SmtpClient client;
+        private Attachment attachment;
+
+        public Mailer(NetworkCredential nc)
+        {
+            client = new SmtpClient("smtp.googlemail.com");
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Credentials = nc;
+            client.SendCompleted += new SendCompletedEventHandler(SendCompleted);
+        }
+
+
+        public void SendMessage(MailMessage m, ListViewX token)
+        {
+            client.SendAsync(m, token);
+        }
+
+        private static void SendCompleted(Object smtpClient, AsyncCompletedEventArgs @event)
+        {
+            ListViewX token;
+
+            token = (ListViewX)@event.UserState;
+
+            if (@event.Error != null)
+            {
+                token.item.SubItems[6].Text = @event.Error.Message;
+            }
+            else
+            {
+                token.item.SubItems[6].Text = "OK";
+                token.item.Checked = false;
+            }
+            Form1.DeleteMessage(token.rowindex);
+            ((SmtpClient)smtpClient).Dispose();
+        }
+    }
+
+
+    class ListViewX
+    {
+        public ListViewItem item;
+        public int rowindex;
+    }
+}
